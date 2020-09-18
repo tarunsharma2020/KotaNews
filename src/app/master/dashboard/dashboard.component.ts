db8b5ef902673878db1b76a126d9b7e2a2ac520d
TARUN SHARMA <rtdigitalsolution@gmail.com>
2020-09-18 11:34:34 -0500

    ff

--- src/app/master/dashboard/dashboard.component.ts
+++ src/app/master/dashboard/dashboard.component.ts
@@ -7,9 +7,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+ 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+ 
+    public imagePath;
+    imgURL: any;
+    imgURL1: any;
+    imgURL2: any;
+    public message: string;
+   
+    preview(files) {
+      if (files.length === 0)
+        return;
+   
+      var mimeType = files[0].type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.message = "Only images are supported.";
+        return;
+      }
+   
+      var reader = new FileReader();
+      this.imagePath = files;
+      reader.readAsDataURL(files[0]); 
+      reader.onload = (_event) => { 
+        this.imgURL = reader.result; 
+        
+      }
+    }
+
+
+    preview1(files) {
+      if (files.length === 0)
+        return;
+   
+      var mimeType = files[0].type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.message = "Only images are supported.";
+        return;
+      }
+   
+      var reader = new FileReader();
+      this.imagePath = files;
+      console.log('preview1')
+      console.log(this.imagePath)
+      reader.readAsDataURL(files[0]); 
+      reader.onload = (_event) => { 
+        this.imgURL1 = reader.result; 
+        
+      }
+    }
+    preview2(files) {
+      if (files.length === 0)
+        return;
+   
+      var mimeType = files[0].type;
+      if (mimeType.match(/image\/*/) == null) {
+        this.message = "Only images are supported.";
+        return;
+      }
+   
+      var reader = new FileReader();
+      this.imagePath = files;
+      console.log('preview2')
+      console.log(this.imagePath)
+      reader.readAsDataURL(files[0]); 
+      reader.onload = (_event) => { 
+        this.imgURL2 = reader.result; 
+        
+      }
+    }
+
+
+  }
+  
+  
+
